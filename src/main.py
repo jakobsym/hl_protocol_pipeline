37f245efab241e62eee9dc06c391d5582a7e3e9a
Jakob <jakob.symchych@my.utsa.edu>
2025-05-03 12:04:24 -0500

    added logging to main, to track total time to complete batch process

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
+import logging
+import time
 from extract.api_extractor import DefiLlamaAPIExtractor, HyperscanAPIExtractor
 from transform.transformer import DefiLlamaJsonTransformer, HyperScanJsonTransformer
 from utils.logging import config_logging
 
 # init logging
 config_logging()
+logger = logging.getLogger("main")
 
 def main():
+    start_time = time.time()
     hyperliquid_dexs = ["hyperswap", "valantis", "kittenswap-finance", "laminar"]
     # blockchains = ["hyperliquid l1"]
 
@@ -16,12 +20,10 @@ def main():
 
     tokens = hs_api_extractor.fetch_tokens()
     transformed_tokens = hs_json_transformer.transform_token_payload(tokens)
-    print(transformed_tokens)
-    
     
     for protocol in hyperliquid_dexs:
         try:
-            """
+            
             # extract raw data
             protocol_metrics = dl_api_extractor.collect_protocol_metrics(protocol)
 
@@ -30,11 +32,12 @@ def main():
             transformed_metrics = dl_json_transformer.transform_protocol_metrics(protocol_metrics)
             
             # load transformed data into storage
-            """
         except Exception as e:
             print(f"  \nERROR processing {protocol}: {str(e)}")
-        
 
+    elapsed_time = (time.time() - start_time) * 1000
+    logger.info("Completed batch process in %.2fms", elapsed_time)
+            
 if __name__ == "__main__":
     main()
 
