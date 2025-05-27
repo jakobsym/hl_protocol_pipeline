8099ddbd1e218f5c8d0e077b3d3288217ce28639
jakobsym <jakob.symchych@my.utsa.edu>
2025-05-26 20:05:03 -0500

    init of csv loading

---
+++ src/load/csv_loader.py
@@ -0,0 +1,12 @@
+import os
+
+class CSVFileLoader:
+
+    def __init__(self, hl_token_csv_dir: str = "../../data/hl_tokens.csv", hl_protocol_csv_dir = "../../data/hl_protocols.csv"):
+        self.hl_token_csv_dir = hl_token_csv_dir
+        self.hl_protocol_csv_dir = hl_protocol_csv_dir
+
+        self.hl_token_csv_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), hl_token_csv_dir))
+        self.hl_protocol_csv_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), hl_protocol_csv_dir))
+        os.makedirs(self.hl_token_csv_dir, exist_ok=True)
+        os.makedirs(self.hl_protocol_csv_dir, exist_ok=True)
